b6508197cb72698ad41ed1cce1849183a36fb6ee
Siddharth966 <146622482+Siddharth966@users.noreply.github.com>
2024-12-20 16:48:39 +0530

    first commit

--- frequencyOfString.java
+++ frequencyOfString.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class frequencyOfString {
+public class frequencyOfString {
+    public static void main(String[] args) {
+        String input = "Siddharth";
+
+        input = input.toLowerCase();
+
+        int[] frequency = new int[256];
+
+        for (char c : input.toCharArray()) {
+            frequency[c]++;
+        }
+
+        System.out.println("Character Frequency:");
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] > 0 && Character.isLetterOrDigit((char) i)) {
+                System.out.println((char) i + ": " + frequency[i]);
+            }
+        }
+    }
 }
